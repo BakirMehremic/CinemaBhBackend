ec5de625182d9f750c564a4e48d18868606f3ac2
BakirMehremic <bakir.mehremic@stu.ibu.edu.ba>
2026-04-06 01:53:01 +0200

    implementing get nameid

--- src/main/java/com/atlantbh/cinemabh/controller/CityController.java
+++ src/main/java/com/atlantbh/cinemabh/controller/CityController.java
@@ -1,10 +1,23 @@
 package com.atlantbh.cinemabh.controller;
 
+import com.atlantbh.cinemabh.dto.response.NameIdPair;
+import com.atlantbh.cinemabh.service.CityService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/cities")
-public class CityController {}
+public class CityController {
+  private final CityService cityService;
+
+  @GetMapping("/names")
+  public ResponseEntity<List<NameIdPair>> getCityNameIdPairs() {
+    List<NameIdPair> response = cityService.getAllCityNameIdPairs();
+    return ResponseEntity.ok(response);
+  }
+}
---
+++ src/main/java/com/atlantbh/cinemabh/service/CityService.java
@@ -0,0 +1,8 @@
+package com.atlantbh.cinemabh.service;
+
+import com.atlantbh.cinemabh.dto.response.NameIdPair;
+import java.util.List;
+
+public interface CityService {
+  List<NameIdPair> getAllCityNameIdPairs();
+}
