0f12062ecae375e63e2fe9c595687031a24593ff
BakirMehremic <bakir.mehremic@stu.ibu.edu.ba>
2026-04-12 15:55:42 +0200

    added different city ids to venues

--- src/main/resources/db/migration/V3__insert_countries_cities_venues.sql
+++ src/main/resources/db/migration/V3__insert_countries_cities_venues.sql
@@ -16,8 +16,8 @@ VALUES ('New York', 1),
        ('Zenica', 4);
 
 INSERT INTO "venues" ("name", "street", "street_number", "phone", "image_url", "city_id")
-VALUES ('Cineplex', 'Ulica Street', '2', '987-654-3210', 'https://ik.imagekit.io/4oxyrugmfe/cineplex.jpeg', 5),
-       ('Meeting Point', 'Cinema St', '121', '987-654-3210', 'https://ik.imagekit.io/4oxyrugmfe/meeting.jpg', 5),
-       ('Cinema City', 'Kino', '17', '987-654-3210', 'https://ik.imagekit.io/4oxyrugmfe/cinema-city.jpg', 5),
+VALUES ('Cineplex', 'Ulica Street', '2', '987-654-3210', 'https://ik.imagekit.io/4oxyrugmfe/cineplex.jpeg', 6),
+       ('Meeting Point', 'Cinema St', '121', '987-654-3210', 'https://ik.imagekit.io/4oxyrugmfe/meeting.jpg', 7),
+       ('Cinema City', 'Kino', '17', '987-654-3210', 'https://ik.imagekit.io/4oxyrugmfe/cinema-city.jpg', 7),
        ('SFF', 'Figueroa St', '90', '987-654-3210', 'https://ik.imagekit.io/4oxyrugmfe/sff.jpg', 5),
        ('Kino Atlant', 'Ulica St', '11', '987-654-3210', 'https://ik.imagekit.io/4oxyrugmfe/kino.jpg', 5);
