39555ce5a9b6fb6e596f43fbeb71649bb260395c
adnangrbesic <adnangrbesic43@gmail.com>
2026-05-10 23:19:43 +0200

    Add docker configuration and update CORS support

--- src/main/java/com/atlantbh/cinemabh/config/CorsConfig.java
+++ src/main/java/com/atlantbh/cinemabh/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
       public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
-            .allowedOrigins(frontendUrl)
+            .allowedOrigins(frontendUrl.split(","))
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD")
             .allowedHeaders("*")
             .allowCredentials(true);
